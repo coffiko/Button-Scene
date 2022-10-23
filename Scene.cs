ad00c1cd8b738c5c306627d35a929a8cd7406b7f
coffiko <116488972+coffiko@users.noreply.github.com>
2022-10-23 15:39:31 +0500

    button scene ←

---
+++ Scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void loadScene(int sceneid)
+    {
+        SceneManager.LoadScene(sceneid);
+    }
+}
